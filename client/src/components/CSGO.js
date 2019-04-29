009e6a5e5dff1570c5e9681cd62eda9bfa1d0f0e
John Genere <johncgenere@gmail.com>
2019-04-28 23:50:39 -0400

    added fav and unfav buttons to game pages with functions for later api calls

--- client/src/components/CSGO.js
+++ client/src/components/CSGO.js
@@ -6,6 +6,7 @@ class CSGO extends React.Component {
 
     this.onHomeClick = this.onHomeClick.bind(this);
     this.onFavoriteClick = this.onFavoriteClick.bind(this);
+    this.onUnfavoriteClick = this.onUnfavoriteClick.bind(this);
   }
 
   onHomeClick = (event) => {
@@ -17,13 +18,21 @@ class CSGO extends React.Component {
     console.log('favvvvvv');
   }
 
+  onUnfavoriteClick = (event) => {
+    console.log('f that');
+  }
+
   render(){
     return(
       <div>
         <h1 style={{textAlign: 'center', marginTop: '3%'}} onClick= {this.onHomeClick}>eInsider</h1>
         <h3 style={{textAlign: 'center'}}> Image Will Be Here </h3>
-        <h1 style={{marginLeft: '47%'}}> CS:GO
-        <button className="ui button black" style={{marginLeft: '33%'}} onClick={this.onFavoriteClick}><i className="star outline icon yellow" />Favorite</button></h1>
+        <h1 style={{textAlign: 'center'}}> CS:GO </h1>
+        <div className="ui tiny buttons" style={{marginLeft: '42%'}}>
+          <button className="ui button black" onClick={this.onFavoriteClick}><i className="star icon yellow" />Favorite</button>
+          <div className="or"></div>
+          <button className="ui button" onClick={this.onUnfavoriteClick}><i className="star outline icon yellow" />Unfavorite</button>
+        </div>
 
         <div className="ui piled segment" style={{marginLeft: '20%', marginRight: '20%'}}>
           <h4 className="ui header">Schedule</h4>
--- client/src/components/LoL.js
+++ client/src/components/LoL.js
@@ -5,11 +5,21 @@ class LoL extends React.Component {
     super(props);
 
     this.onHomeClick = this.onHomeClick.bind(this);
+    this.onFavoriteClick = this.onFavoriteClick.bind(this);
+    this.onUnfavoriteClick = this.onUnfavoriteClick.bind(this);
   }
 
   onHomeClick = (event) => {
     console.log('Go home!');
     window.location.replace('/');
+  }
+
+  onFavoriteClick = (event) => {
+    console.log('favvvvvv');
+  }
+
+  onUnfavoriteClick = (event) => {
+    console.log('f that');
   }
 
   render(){
@@ -18,6 +28,11 @@ class LoL extends React.Component {
         <h1 style={{textAlign: 'center', marginTop: '3%'}} onClick={this.onHomeClick}>eInsider</h1>
         <h3 style={{textAlign: 'center'}}> Image Will Be Here </h3>
         <h1 style={{textAlign: 'center'}}> League of Legends </h1>
+        <div className="ui tiny buttons" style={{marginLeft: '42%'}}>
+          <button className="ui button black" onClick={this.onFavoriteClick}><i className="star icon yellow" />Favorite</button>
+          <div className="or"></div>
+          <button className="ui button" onClick={this.onUnfavoriteClick}><i className="star outline icon yellow" />Unfavorite</button>
+        </div>
 
         <div className="ui piled segment" style={{marginLeft: '20%', marginRight: '20%'}}>
           <h4 className="ui header">Schedule</h4>
