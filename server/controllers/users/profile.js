e255359a313f0707794e2f9699e5fbd3286cf861
mnurilov <michaelnurilov67@gmail.com>
2019-05-01 21:16:02 -0400

    Added basic auth to registerand added security to get profile

--- server/controllers/users/profile.js
+++ server/controllers/users/profile.js
@@ -1,22 +1,59 @@
+const _ = require('lodash');
+
 const setup = (context) => {
+  const user = context.models.user;
+
   const logEndpoint = (req, res, next) => {
     console.log("You have hit [GET] /users/profile endpoint");
-    const user = context.models.user;
-    user.findAll()
-    .then((res) => {
-      console.log(res);
-    })
     next();
+  };
+  
+  const sanitizeParams = (req, res, next) => {
+    if (_.isEmpty(req.body.username)) {
+      return res
+        .status(400)
+        .send('cannot find desired username in req.body');
+    }
+
+    req.clean = {
+      username: req.body.username
+    }
+    
+    next();
+  }
+
+  const findUser = (req, res, next) => {
+    user.findOne({ 
+      where: { 
+        username: req.clean.username 
+      } 
+    })
+      .then(user => {
+        if (_.isEmpty(user)) {
+          throw new Error('user not found');
+        }
+
+        req.clean.user = user;
+        next();
+      })
+      .catch(err => {
+        return res
+          .status(400)
+          .send(`user not found with username: ${req.clean.username}\n`)
+      });
   };
 
   const sendResponse = (req, res, next) => {
+    console.log("Sending back the following json:\n" + JSON.stringify(user, null, 2));
     res
     .status(200)
-    .send('Welcome to profile');
+    .json(req.clean.user);
   };
   
   return [
     logEndpoint,
+    sanitizeParams,
+    findUser,
     sendResponse
   ];
 };
--- server/controllers/users/register.js
+++ server/controllers/users/register.js
@@ -1,23 +1,80 @@
+const _ = require('lodash');
+
 const setup = (context) => {
+  const user = context.models.user;
+  const models = context.models;
+
   const logEndpoint = (req, res, next) => {
     console.log("You have hit [POST] /users/register endpoint");
-    console.log(JSON.stringify(req.body, null, 2));
-    const models = context.models;
-    models.sequelize.query(`INSERT INTO "users" (username, password) VALUES ('${req.body.username}', '${req.body.password}');`)
-    .then((res) => {
-      console.log(res);
-    })
     next();
   };
+
+  const sanitizeParams = (req, res, next) => {
+    if (_.isEmpty(req.body.username)) {
+      console.log('cannot create user without username');
+      return res
+        .status(400)
+        .send('cannot create user without username');
+    }
+
+    if (_.isEmpty(req.body.password)) {
+      console.log('cannot create user without password');
+      return res
+        .status(400)
+        .send('cannot create user without password');
+    }
+
+    req.clean = {
+      username: req.body.username,
+      password: req.body.password
+    }
+
+    next();
+  }
+  
+  const findUser = (req, res, next) => {
+    user.findOne({ 
+      where: { 
+        username: req.clean.username 
+      } 
+    })
+      .then(user => {
+        if (!_.isEmpty(user)) {
+          throw new Error(`user with the username '${req.clean.username}' already exists`);
+        }
+
+        next();
+      })
+      .catch(err => {
+        console.log(`user with the username '${req.clean.username}' already exists`);
+        return res
+          .status(400)
+          .send(`${err.message}`)
+      });
+  };
+
+  const createUser = (req, res, next) => {
+    models.sequelize.query(`INSERT INTO "users" (username, password) VALUES ('${req.body.username}', '${req.body.password}');`)
+    .then(() => {
+      console.log('user inserted into the database!');
+      next();
+    })
+    .catch(err => {
+      console.log(`user couldn't be inserted into the database because ${err.message}`);
+    })
+  }
 
   const sendResponse = (req, res, next) => {
     res
     .status(200)
-    .send('Welcome to register');
-  };
+    .send('user inserted into the database!')
+  }
   
   return [
     logEndpoint,
+    sanitizeParams,
+    findUser,
+    createUser,
     sendResponse
   ];
 };
